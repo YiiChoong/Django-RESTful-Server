4a8affb4fa404df9bf29a1b7e8b7c5c34e1cbdb1
Yii Choong Kaw <yiichoongk@gmail.com>
2022-12-23 13:11:41 -0800

    commit

--- restAPI/myapi/urls.py
+++ restAPI/myapi/urls.py
@@ -3,6 +3,7 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
+# Provide prefix and viewset to register the router
 router.register(r'cars', views.CarViewSet)
 router.register(r'trucks', views.TruckViewSet)
 router.register(r'boats', views.BoatViewSet)
